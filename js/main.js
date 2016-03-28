47fcbea91ed62f6c08be15167ed4a0cb7db42ea8
Francis Wang <go717franciswang@hotmail.com>
2016-03-27 23:53:22 -0700

    Add a ball

--- js/main.js
+++ js/main.js
@@ -4,6 +4,17 @@ PinBall.Main = function(game) {
 
 PinBall.Main.prototype = {
     create: function() {
-        console.log('game started');
+        var circle = this.make.graphics(0, 0);
+        circle.lineStyle(8, 0xFF0000, 0.8);
+        circle.beginFill(0xFF700B, 1);
+        circle.drawCircle(-50, -50, 10);
+        circle.endFill();
+
+        this.ball = this.add.sprite(
+            this.world.centerX,
+            this.world.centerY,
+            circle.generateTexture()
+        );
+        this.ball.anchor.set(0.5);
     }
 }
