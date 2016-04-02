c369cacc39a5b0c56f8a463a0361bc76e373dede
Francis Wang <go717franciswang@hotmail.com>
2016-04-01 22:04:37 -0700

    Make arms respond to keyboard

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ var Pinball;
             _super.apply(this, arguments);
         }
         Main.prototype.create = function () {
+            var _this = this;
             var circle = this.make.graphics(0, 0);
             circle.lineStyle(8, 0xFF0000, 0.8);
             circle.beginFill(0xFF700B, 1);
@@ -24,12 +25,36 @@ var Pinball;
             rect.beginFill(0xFF700B, 1);
             rect.drawRect(-50, -50, 100, 20);
             rect.endFill();
-            this.leftArm = this.add.sprite(this.world.centerX - 120, this.world.centerY, rect.generateTexture());
+            this.leftArm = this.add.sprite(this.world.centerX - 120, this.world.height - 100, rect.generateTexture());
             this.leftArm.anchor.set(0.1, 0.5);
             this.leftArm.angle = 45;
-            this.leftArm = this.add.sprite(this.world.centerX + 120, this.world.centerY, rect.generateTexture());
-            this.leftArm.anchor.set(0.9, 0.5);
-            this.leftArm.angle = -45;
+            this.rightArm = this.add.sprite(this.world.centerX + 120, this.world.height - 100, rect.generateTexture());
+            this.rightArm.anchor.set(0.9, 0.5);
+            this.rightArm.angle = -45;
+            var leftKey = this.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+            leftKey.onDown.add(function () { _this.leftDown = true; });
+            leftKey.onUp.add(function () { _this.leftDown = false; });
+            var rightKey = this.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+            rightKey.onDown.add(function () { _this.rightDown = true; });
+            rightKey.onUp.add(function () { _this.rightDown = false; });
+            this.leftDown = false;
+            this.rightDown = false;
+        };
+        Main.prototype.update = function () {
+            if (this.leftDown) {
+                this.leftArm.angle -= 15;
+            }
+            else {
+                this.leftArm.angle += 15;
+            }
+            this.leftArm.angle = Phaser.Math.clamp(this.leftArm.angle, -45, 45);
+            if (this.rightDown) {
+                this.rightArm.angle += 15;
+            }
+            else {
+                this.rightArm.angle -= 15;
+            }
+            this.rightArm.angle = Phaser.Math.clamp(this.rightArm.angle, -45, 45);
         };
         return Main;
     }(Phaser.State));
--- ts/main.ts
+++ ts/main.ts
@@ -7,6 +7,8 @@ module Pinball {
         ball: Phaser.Sprite;
         leftArm: Phaser.Sprite;
         rightArm: Phaser.Sprite;
+        leftDown: boolean;
+        rightDown: boolean;
 
         create() {
             var circle = this.make.graphics(0, 0);
@@ -31,20 +33,46 @@ module Pinball {
 
             this.leftArm = this.add.sprite(
                 this.world.centerX - 120, 
-                this.world.centerY,
+                this.world.height - 100,
                 rect.generateTexture()
             );
             this.leftArm.anchor.set(0.1, 0.5);
             this.leftArm.angle = 45;
 
-            this.leftArm = this.add.sprite(
+            this.rightArm = this.add.sprite(
                 this.world.centerX + 120, 
-                this.world.centerY,
+                this.world.height - 100,
                 rect.generateTexture()
             );
-            this.leftArm.anchor.set(0.9, 0.5);
-            this.leftArm.angle = -45;
+            this.rightArm.anchor.set(0.9, 0.5);
+            this.rightArm.angle = -45;
 
+            var leftKey = this.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+            leftKey.onDown.add(() => { this.leftDown = true });
+            leftKey.onUp.add(() => { this.leftDown = false });
+
+            var rightKey = this.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+            rightKey.onDown.add(() => { this.rightDown = true });
+            rightKey.onUp.add(() => { this.rightDown = false });
+
+            this.leftDown = false;
+            this.rightDown = false;
+        }
+
+        update() {
+            if (this.leftDown) {
+                this.leftArm.angle -= 15;
+            } else {
+                this.leftArm.angle += 15;
+            }
+            this.leftArm.angle = Phaser.Math.clamp(this.leftArm.angle, -45, 45);
+
+            if (this.rightDown) {
+                this.rightArm.angle += 15;
+            } else {
+                this.rightArm.angle -= 15;
+            }
+            this.rightArm.angle = Phaser.Math.clamp(this.rightArm.angle, -45, 45);
         }
     }
 }
